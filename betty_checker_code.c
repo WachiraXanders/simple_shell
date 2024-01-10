d7d13b5cc6cb869c250fce974f0a75283d8ec7d0
Xanders Wachira <muhindia66@gmail.com>
2024-01-09 20:44:02 -0800

    correctional update.

---
+++ betty_checker_code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Means that there will be no return statement hence the void.
+ *
+ *Return: No return statement.
+ */
+int main(void)
+{
+	printf("Hello, World.\n");
+}
